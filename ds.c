e8a80c5fbf957a9c280c477059b9e5fe53437932
Sakshi Sharma <155537318+Sakshisharma87@users.noreply.github.com>
2024-08-23 08:52:33 +0530

    Update ds.c

--- ds.c
+++ ds.c
@@ -17,19 +17,19 @@ struct TreeNode {
 typedef struct TreeNode TreeNode;
 
 // Function prototypes
-TreeNode* createTreeNode(int value);
+TreeNode* createNode(int value);
 TreeNode* insertBST(TreeNode* root, int value);
-void inorderTraversal(TreeNode* root);
-void handleArray(int size);
+void inorder(TreeNode* root);
+void handleArray(int n);
 void handleLinkedList();
 void handleStack();
 void handleQueue();
-int push(int stackTop, int maxStackSize, int stack[]);
+int push(int top, int max, int stack[]);
 int pop(int stack[], int top);
 void traverseStack(int top, int stack[]);
 
 int main() {
-    int choice, arraySize, stackTop = -1;
+    int choice, n, top = -1;
     TreeNode* root = NULL;
     int stack[100];
 
@@ -40,9 +40,9 @@ int main() {
         switch (choice) {
             case 1:
                 printf("Enter the number of elements (not more than 100): ");
-                scanf("%d", &arraySize);
-                if (arraySize > 0 && arraySize <= 100) {
-                    handleArray(arraySize);
+                scanf("%d", &n);
+                if (n > 0 && n <= 100) {
+                    handleArray(n);
                 } else {
                     printf("Invalid size! Must be between 1 and 100.\n");
                 }
@@ -53,7 +53,7 @@ int main() {
                 break;
 
             case 3:
-                handleStack(stackTop, stack);
+                handleStack(top, stack);
                 break;
 
             case 4:
@@ -62,22 +62,22 @@ int main() {
 
             case 5:
                 printf("Enter value of root node: ");
-                int rootValue;
-                scanf("%d", &rootValue);
-                root = createTreeNode(rootValue);
+                int rootVal;
+                scanf("%d", &rootVal);
+                root = createNode(rootVal);
 
-                char continueInsertion = 'y';
-                while (continueInsertion == 'y' || continueInsertion == 'Y') {
+                char cont = 'y';
+                while (cont == 'y' || cont == 'Y') {
                     printf("Enter data to insert: ");
-                    int value;
-                    scanf("%d", &value);
-                    root = insertBST(root, value);
+                    int val;
+                    scanf("%d", &val);
+                    root = insertBST(root, val);
 
                     printf("Do you want to continue insertion? (y/n): ");
-                    scanf(" %c", &continueInsertion);
+                    scanf(" %c", &cont);
                 }
                 printf("Inorder traversal of BST:\n");
-                inorderTraversal(root);
+                inorder(root);
                 break;
 
             case 0:
@@ -94,7 +94,7 @@ int main() {
 }
 
 // Function to create a new node in a Binary Search Tree (BST)
-TreeNode* createTreeNode(int value) {
+TreeNode* createNode(int value) {
     TreeNode* node = (TreeNode*)malloc(sizeof(TreeNode));
     node->data = value;
     node->left = NULL;
@@ -105,7 +105,7 @@ TreeNode* createTreeNode(int value) {
 // Function to insert a value into a Binary Search Tree (BST)
 TreeNode* insertBST(TreeNode* root, int value) {
     if (root == NULL) {
-        return createTreeNode(value);
+        return createNode(value);
     } else if (value > root->data) {
         root->right = insertBST(root->right, value);
     } else {
@@ -115,23 +115,23 @@ TreeNode* insertBST(TreeNode* root, int value) {
 }
 
 // Function for inorder traversal of a Binary Search Tree (BST)
-void inorderTraversal(TreeNode* root) {
+void inorder(TreeNode* root) {
     if (root != NULL) {
-        inorderTraversal(root->left);
+        inorder(root->left);
         printf("%d\n", root->data);
-        inorderTraversal(root->right);
+        inorder(root->right);
     }
 }
 
 // Function to handle array operations
-void handleArray(int arraySize) {
+void handleArray(int n) {
     int i, arr[100];
-    printf("Enter %d elements:\n", arraySize);
-    for (i = 0; i < arraySize; i++) {
+    printf("Enter %d elements:\n", n);
+    for (i = 0; i < n; i++) {
         scanf("%d", &arr[i]);
     }
     printf("The array elements are:\n");
-    for (i = 0; i < arraySize; i++) {
+    for (i = 0; i < n; i++) {
         printf("%d\n", arr[i]);
     }
 }
@@ -139,9 +139,9 @@ void handleArray(int arraySize) {
 // Function to handle linked list operations
 void handleLinkedList() {
     struct ListNode *head = NULL, *temp, *newNode;
-    char continueInsertion = 'y';
+    char cont = 'y';
 
-    while (continueInsertion == 'y' || continueInsertion == 'Y') {
+    while (cont == 'y' || cont == 'Y') {
         newNode = (struct ListNode*)malloc(sizeof(struct ListNode));
         printf("Enter data: ");
         scanf("%d", &newNode->data);
@@ -158,7 +158,7 @@ void handleLinkedList() {
         }
 
         printf("Do you want to continue adding nodes? (y/n): ");
-        scanf(" %c", &continueInsertion);
+        scanf(" %c", &cont);
     }
 
     printf("The linked list is:\n");
@@ -170,9 +170,9 @@ void handleLinkedList() {
 }
 
 // Function to handle stack operations
-void handleStack(int stackTop, int stack[]) {
+void handleStack(int top, int stack[]) {
     int choice;
-    int maxStackSize = 100;
+    int max = 100;
 
     while (1) {
         printf("Stack operations:\n1. Push\n2. Pop\n3. Traverse\n0. Exit\n");
@@ -180,20 +180,20 @@ void handleStack(int stackTop, int stack[]) {
 
         switch (choice) {
             case 1:
-                stackTop = push(stackTop, maxStackSize, stack);
+                top = push(top, max, stack);
                 break;
 
             case 2:
-                if (stackTop >= 0) {
-                    printf("The popped item is %d\n", pop(stack, stackTop));
-                    stackTop--;
+                if (top >= 0) {
+                    printf("The popped item is %d\n", pop(stack, top));
+                    top--;
                 } else {
                     printf("Stack underflow.\n");
                 }
                 break;
 
             case 3:
-                traverseStack(stackTop, stack);
+                traverseStack(top, stack);
                 break;
 
             case 0:
@@ -207,17 +207,17 @@ void handleStack(int stackTop, int stack[]) {
 }
 
 // Function to push an element onto the stack
-int push(int stackTop, int maxStackSize, int stack[]) {
-    int value;
-    if (stackTop == maxStackSize - 1) {
+int push(int top, int max, int stack[]) {
+    int val;
+    if (top == max - 1) {
         printf("Stack overflow.\n");
-        return stackTop;
+        return top;
     } else {
         printf("Enter element to push: ");
-        scanf("%d", &value);
-        stackTop++;
-        stack[stackTop] = value;
-        return stackTop;
+        scanf("%d", &val);
+        top++;
+        stack[top] = val;
+        return top;
     }
 }
 
@@ -232,12 +232,12 @@ int pop(int stack[], int top) {
 }
 
 // Function to traverse the stack
-void traverseStack(int stackTop, int stack[]) {
-    if (stackTop == -1) {
+void traverseStack(int top, int stack[]) {
+    if (top == -1) {
         printf("Stack is empty.\n");
     } else {
         printf("The stack elements are:\n");
-        for (int i = stackTop; i >= 0; i--) {
+        for (int i = top; i >= 0; i--) {
             printf("%d\n", stack[i]);
         }
     }
@@ -246,7 +246,7 @@ void traverseStack(int stackTop, int stack[]) {
 // Function to handle queue operations
 void handleQueue() {
     struct ListNode *front = NULL, *rear = NULL, *temp, *newNode;
-    char continueInsertion = 'y';
+    char cont = 'y';
     int choice;
 
     while (1) {
@@ -278,227 +278,26 @@ void handleQueue() {
                     free(temp);
 
                     if (front == NULL) {
-                        rear = NULL# include <stdio.h>
-# include<stdlib.h>
-# include<string.h>
-void element(){
-    
-}
-	struct s{
-		int info;
-		struct s *next;
-	};
-    struct tre{
-        int info;
-        struct tre *lc;
-        struct tre *rc ;
-    };
-    typedef struct tre t;
-t* new(int x){
-      t *n;
-      n=(t*)malloc(sizeof(t));
-     n->info=x;
-     n->lc=NULL;
-     n->rc=NULL;
-     return n;
-}
-t* insert(t* r,int x){
-    if(r==NULL){
-        return new(x);
-    }
-    else if(x>r->info){
-      r->rc=insert(r->rc,x);
-    }
-    else{
-        r->lc=insert(r->lc,x);
-    }
-    return r;
-}
-void array(int a){
-   int i,x[100];
-   printf("enter data:");
-   for(i=0;i<a;i++){
-     scanf("%d",&x[i]);
-   }
-   printf("the array is:");
-   for(i=0;i<a;i++){
-     printf("%d\n",x[i]);
-   }
-}
-int link(int x, int y){
-    char ch;
-    typedef struct s node;
-     node *ptr,*start,*next,*f,*r,*n;
-   ptr=NULL;
-    do{
-    	n=(node*)malloc(sizeof(node));
-    	printf("enter data:");
-    	scanf("%d",&n->info);
-    	if(ptr!=NULL){
-    		ptr->next=n;
-    		ptr=ptr->next;
-		}
-		else{
-			start=n;
-			ptr=n;
-		}
-		scanf("%c",&ch);
-		printf("do you want to continue(enter y/n):");
-		scanf("%c",&ch);
-	}while(ch=='y');
-	
-	ptr->next=NULL;
-   if(x==2){
-        printf("\nthe linklist is:");
-	}
-   
-   if(x==4){
-    printf("the queue is:");
-    f=start;
-    r=ptr;
-    
-   }
-    ptr=start;
-	while(ptr!=NULL){
-		printf("%d\n",ptr->info);
-		ptr=ptr->next;
-    }
-    if(x==4 &&y==2){
-       ptr=f;
-       f=f->next;
-       ptr=start;
-       printf("the queue after deletion is:");
-       ptr=f;
-	while(ptr!=NULL){
-		printf("%d\n",ptr->info);
-		ptr=ptr->next;
-    }
-      free(ptr);
-    }
-}
-int  push(int t,int m,int s[]){
-     int i;
-     if(t==m-1){
-        printf("stack overflow");
-        exit(0);
-     }
-     else{
-        printf("enter element to insert:");
-        scanf("%d",&i);
-        t=t+1;
-        s[t]=i;
-        return t;
-     }
+                        rear = NULL;
+                    }
+                }
+                break;
 
-}
-int pop(int s[],int t){
-    int i;
-    if(t==-1){
-        printf("stack underflow");
-        exit(0);
-    }
-    else{
-        i=s[t];
-        t=t-1;
-        return i;
-    }
-
-
-}
-void transverse(int t,int s[]){
-    int i;
-    if(t==-1){
-        printf("stack underflow");
-        exit(0);
-    }
-    else{
-        printf("the stack is:");
-        for (i = t; i >=0; i--)
-        {
-            printf("%d\n",s[i]);
-        }
-        
-    }
-}
-void inorder(t *r){
-    // printf("the inorder transversal of tree :\n");
-    if(r!=NULL){
-        inorder(r->lc);
-        printf("%d\n",r->info);
-        inorder(r->rc);
-    }
-
-}
-int main(){
-    int a,b=1,n=1,i,z;
-    struct s *p,*tr;
-    t *root;
-    char ch='y';
-    printf("select data structure:\n1.array\n2.linklist\n3.stack\n4.queue\n5.tree(BST)\n");
-    scanf("%d",&a);
-    switch(a){
-        case 1:
-        printf("enter the no. of element:(not more than 100 elements)");
-        scanf("%d",&n);
-        array(n);
-        break;
-        case 2:
-         link(a,b);
-         break;
-         case 3:
-        //   while(n==1){
-         printf("(stack should not exceed 100 elements)\n");
-         printf("select operation:-\n");
-        //  scanf("%d",&b);
-         int st[100];
-         int top=-1;
-         int max=100;
-         while(n==1){
-             printf("1.push\n2.pop\n3.traverse\n");
-         scanf("%d",&b);
-         switch (b)
-         {
-         case 1:
-           top= push(top,max,st);
-            break;
-         case 2:
-          printf("the deleted item is %d\n",pop(st,top));
-          top=top-1;
-          break;
             case 3:
-            transverse(top,st);
-            break;
-         default:
-         printf("incorrect choice...");
-            break;
-         }
-         printf("do you want to continue (1/0):");
-         scanf("%d",&n);
+                printf("The queue elements are:\n");
+                temp = front;
+                while (temp != NULL) {
+                    printf("%d\n", temp->data);
+                    temp = temp->next;
+                }
+                break;
 
-         } 
-         break;
-         case 4:
-          printf("select operation\n1.insertion only\n2.insertion and deletion\n");
-         scanf("%d",&b);
-         link(a,b);
-         break;
-         case 5:
-         printf("enter value of root node:");
-         scanf("%d",&n);
-          root=new(n);
-         scanf("%c",&ch);
-         printf("do you want to continue insertion(y/n):");
-         scanf("%c",&ch);
-        while(ch=='y'){
-          printf("enter data:");
-          scanf("%d",&z);
-          insert(root,z);
-           scanf("%c",&ch);
-          printf("do you want to continue insertion(y/n):");
-         scanf("%c",&ch);
+            case 0:
+                return;
+
+            default:
+                printf("Incorrect choice. Please select a valid option.\n");
+                break;
         }
-    printf("the inorder transversal of BST :\n");
-         inorder(root);
-         break;
     }
 }
